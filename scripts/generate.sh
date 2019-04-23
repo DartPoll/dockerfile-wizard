3897b3cf63c82d10533d715473a1c1cb70e1a4ce
Robin Jayaswal <robin.e.jayaswal.18@dartmouth.edu>
2019-04-23 14:42:15 -0700

    remove double comma

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -33,7 +33,7 @@ wget http://download.redis.io/releases/redis-$REDIS_VERSION.tar.gz && \
 tar xzf redis-$REDIS_VERSION.tar.gz && \
 cd redis-$REDIS_VERSION && \
 make && \
-cd ..""
+cd .."
 
 
 if [ ! -e $RUBY_VERSION_NUM ] ; then
